ca6907c10abbf5d65172529679fad65330f25957
Katherine Sewell <SEWELLKA@ie.ibm.com>
2017-02-15 17:42:12 +0000

    add type

--- public/pickerConsumer.js
+++ public/pickerConsumer.js
@@ -90,10 +90,11 @@ function resultHandler(e) {
     if (result.assetType == 'image'){
         resultResource = '<div>'+resultUrl+'</div><img width="150"  src="'+ deliveryUrl +'"><div><pre>' + json + '</div>'
     } else if (result.assetType == 'video'){
-        resultResource = '<div>'+resultUrl+'</div><video width="150"  src="'+ deliveryUrl +'"></video><div><pre>' + json + '</div>'
+        resultResource = '<div>'+resultUrl+'</div><video width="150"  type="{{asset.mediaType}}" controls  src="'+ deliveryUrl +'"></video><div><pre>' + json + '</div>'
     } else {
         resultResource = '<div>'+resultUrl+'</div><div><pre>' + json + '</div>'
     }
     $('#result').html(resultResource);
+
 }
 
